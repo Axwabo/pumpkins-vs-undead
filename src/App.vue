178770b2e0720d94e459d8b09f058cec6127d3d4
Axwabo <axwabo@gmail.com>
2025-11-01 17:21:40 +0100

    splash screen

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,12 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue"
-import PWABadge from "./components/PWABadge.vue"
+import { ref } from "vue";
+import PWABadge from "./components/PWABadge.vue";
+import SplashScreen from "./components/SplashScreen.vue";
+
+const splash = ref(true);
 </script>
 
 <template>
-    <div>
-        <a href="https://vite.dev" target="_blank">
-            <img src="/favicon.svg" class="logo" alt="Pumpkins vs. Undead logo" />
-        </a>
-        <a href="https://vuejs.org/" target="_blank">
-            <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-        </a>
-    </div>
-    <HelloWorld msg="Pumpkins vs. Undead" />
+    <SplashScreen v-if="splash" v-on:click="splash = false" />
     <PWABadge />
 </template>
-
-<style scoped>
-.logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-}
-
-.logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-    filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
---
+++ src/components/SplashScreen.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const emit = defineEmits([ "click" ]);
+
+const tilt = window.innerWidth < window.innerHeight;
+</script>
+
+<template>
+    <div id="splash">
+        <h1>Pumpkins vs. Undead</h1>
+        <template v-if="tilt">
+            <h1>📱🔄</h1>
+            <h2>For a better experience, set your device to be landscape-oriented</h2>
+        </template>
+        <button onclick="document.body.requestFullscreen();">Fullscreen</button>
+        <button v-on:click="emit('click')">Play</button>
+    </div>
+</template>
+
+<style scoped>
+#splash {
+    display: grid;
+    place-items: center;
+    gap: 1rem;
+    text-align: center;
+}
+</style>
