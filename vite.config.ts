06135ee71f6a88803d58778c415691f7d073cf65
Axwabo <axwabo@gmail.com>
2025-11-03 00:10:48 +0100

    set base path + deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 
+const base = "/pumpkins-vs-undead/";
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [ vue(), VitePWA({
@@ -32,6 +34,9 @@ export default defineConfig({
             navigateFallback: "index.html",
             suppressWarnings: true,
             type: "module"
-        }
-    }) ]
+        },
+
+        base
+    }) ],
+    base
 })
