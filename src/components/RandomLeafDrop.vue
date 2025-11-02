6ae1af28b6364395bfde0a8a99f4584b39192395
Axwabo <axwabo@gmail.com>
2025-11-03 00:04:29 +0100

    leaf playback rate

--- src/components/RandomLeafDrop.vue
+++ src/components/RandomLeafDrop.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-import { ref, useTemplateRef } from "vue";
+import { ref, useTemplateRef, watch } from "vue";
 import useAnimationFrame from "../composables/useAnimationFrame.ts";
 import useGameStore from "../stores/gameStore.ts";
+import { storeToRefs } from "pinia";
 
 const { earn } = useGameStore();
+
+const { speed } = storeToRefs(useGameStore());
 
 const leaf = useTemplateRef("leaf");
 
@@ -11,12 +14,17 @@ const random = ref(0);
 
 let remaining = randomize();
 
+let animation: Animation | null = null;
+
 useAnimationFrame(seconds => {
     if ((remaining -= seconds) > 0)
         return;
     remaining = randomize();
     random.value = Math.random();
-    leaf.value?.animate([ { top: "-1em", visibility: "visible" }, { top: "100vh" } ], { duration: 5000, easing: "linear" });
+    if (!leaf.value)
+        return;
+    animation = leaf.value.animate([ { top: "-1em", visibility: "visible" }, { top: "100vh" } ], { duration: 5000, easing: "linear" });
+    animation.updatePlaybackRate(speed.value);
 });
 
 function randomize() {
@@ -27,6 +35,8 @@ function collect() {
     earn(20);
     leaf.value?.getAnimations()[0]?.cancel();
 }
+
+watch(speed, value => animation?.updatePlaybackRate(value));
 </script>
 
 <template>
