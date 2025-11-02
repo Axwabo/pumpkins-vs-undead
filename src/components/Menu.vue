9193e73997becc4cfb10f6a8088e7eac6f2e568a
Axwabo <axwabo@gmail.com>
2025-11-02 07:58:22 +0100

    begin menu

---
+++ src/components/Menu.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { useTemplateRef } from "vue";
+
+const menuDialog = useTemplateRef("dialog");
+
+defineExpose({
+    open() {
+        menuDialog.value?.showModal();
+    }
+});
+</script>
+
+<template>
+    <dialog ref="dialog">
+        <button v-on:click="menuDialog?.close()">Close</button>
+    </dialog>
+</template>
+
+<style scoped>
+
+</style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,13 +1,26 @@
 <script setup lang="ts">
+import Menu from "./Menu.vue";
+import { useTemplateRef } from "vue";
 
+const menuDialog = useTemplateRef("menu");
 </script>
 
 <template>
-    <nav>
-
+    <nav id="nav">
+        <div class="separator"></div>
+        <button v-on:click="menuDialog?.open()" id="openMenu">Menu</button>
     </nav>
+    <Menu ref="menu" />
 </template>
 
 <style scoped>
+#nav {
+    display: flex;
+    padding: 0.5rem;
+    gap: 0.5rem;
+}
 
+.separator {
+    flex: 1;
+}
 </style>
